6707eea8b01ba07d0a06e4a8c31b65d397d5a980
Dusty Phillips <dusty@archlinux.ca>
2010-05-11 13:24:59 -0600

    Add initial classlist model.

---
+++ programming_lab/classlist/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from classlist.models import ClassList
+
+admin.site.register(ClassList)
--- programming_lab/classlist/models.py
+++ programming_lab/classlist/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ClassList(models.Model):
+    class_name = models.CharField(max_length=128)
+    class_number = models.CharField(max_length=8)
+    participants = models.ManyToManyField(User, related_name="classes")
+
+    def __unicode__(self):
+        return self.class_name
