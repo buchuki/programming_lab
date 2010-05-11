24faeb27d0b72d41c7deb2a1353e02dcb9386399
Dusty Phillips <dusty@archlinux.ca>
2010-05-11 12:51:07 -0600

    Import localsettings.

--- programming_lab/settings.py
+++ programming_lab/settings.py
@@ -1,4 +1,5 @@
-# Django settings for programming_lab project.
+import os.path
+PROJECT_HOME = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,9 +12,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'au_vpl',                      # Or path to database file if using sqlite3.
+        'USER': 'au_vpl',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -92,3 +93,5 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
 )
+
+#from localsettings import *
