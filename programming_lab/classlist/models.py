70276afbd8e11249998983d8eb0fa6ae34435128
Dusty Phillips <dusty@archlinux.ca>
2010-05-14 16:54:55 -0600

    Ensure class_number is unique.

--- programming_lab/classlist/models.py
+++ programming_lab/classlist/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class ClassList(models.Model):
     class_name = models.CharField(max_length=128)
-    class_number = models.CharField(max_length=8)
+    class_number = models.CharField(max_length=8, unique=True)
     participants = models.ManyToManyField(User, related_name="classes", blank=True)
 
     def __unicode__(self):
