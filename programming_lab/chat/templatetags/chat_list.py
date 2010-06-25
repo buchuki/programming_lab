4ee32f08a2e47e4a23bda5d830db05d849c9bd6b
Dusty Phillips <dusty@archlinux.ca>
2010-06-24 20:25:12 -0600

    Show gravatar in chat list.

--- programming_lab/chat/templatetags/chat_list.py
+++ programming_lab/chat/templatetags/chat_list.py
@@ -1,5 +1,7 @@
+import hashlib
 from django import template
 from django.utils.safestring import mark_safe
+from django.utils.html import escape
 
 from chat.models import ChatMessage
 
@@ -17,3 +19,9 @@ def chat_status(sender, receiver):
     elif sender.get_profile().is_online():
         return "online"
     return "offline"
+
+@register.simple_tag
+def gravatar(email, size=32):
+    digest = hashlib.new('md5', email.lower()).hexdigest()
+    url = 'http://www.gravatar.com/avatar/%s?s=%s&d=identicon' % (digest, size)
+    return """<img src="%s" height="%s" width="%s"/>""" % (escape(url), size, size)
