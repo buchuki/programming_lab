742b04cf923348e9706e15490ef878f3b318de2c
Dusty Phillips <dusty@archlinux.ca>
2010-09-16 12:59:49 -0600

    Ensure tutors and instructors only see projects assigned to their classes.

--- programming_lab/project/admin.py
+++ programming_lab/project/admin.py
@@ -5,4 +5,12 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = "name", "owner", "classlist", "lab", "project_type"
     list_filter = "owner", "classlist", "lab"
 
+    def queryset(self, request):
+        all_projects = super(ProjectAdmin, self).queryset(request)
+        if request.user.is_superuser:
+            return all_projects
+        return all_projects.filter(
+                classlist__instructor=request.user) | all_projects.filter(
+                        classlist__classtutor__tutor=request.user)
+
 admin.site.register(Project, ProjectAdmin)
