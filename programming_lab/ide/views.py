2e008b301aa76418a7bfa4db83d73f35dd6683ee
Dusty Phillips <dusty@archlinux.ca>
2010-10-14 09:46:04 -0600

    Add a registration form to the index page.

--- programming_lab/ide/views.py
+++ programming_lab/ide/views.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from registration.forms import RegistrationForm
 
 @login_required
 def main_ide(request):
     return render_to_response("ide/main_ide.html", RequestContext(request, {}))
 
 def index(request):
-    return render_to_response("index.html", RequestContext(request, {}))
+    registration_form = RegistrationForm()
+    return render_to_response("index.html", RequestContext(request, 
+        {"registration_form": registration_form}))
 
 def about(request):
     return render_to_response("about.html", RequestContext(request, {}))
