5cf47e4757915dd9d90ac3b9374a13fb5f46eac4
Dusty Phillips <dusty@archlinux.ca>
2010-11-08 14:36:04 -0700

    Admin improvements.

--- programming_lab/project/admin.py
+++ programming_lab/project/admin.py
@@ -4,6 +4,7 @@ from project.models import Project
 class ProjectAdmin(admin.ModelAdmin):
     list_display = "name", "owner", "classlist", "lab", "project_type"
     list_filter = "owner", "classlist", "lab"
+    search_fields = ["name", "classlist__class_name", "lab__name", "owner__username"]
 
     def queryset(self, request):
         all_projects = super(ProjectAdmin, self).queryset(request)
