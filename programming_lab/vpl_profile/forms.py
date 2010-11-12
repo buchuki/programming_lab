15c6f9aa64cdb53217560e2eb8106e2353d55d68
Dusty Phillips <dusty@archlinux.ca>
2010-11-12 13:22:23 -0700

    Don't allow duplicate e-mails.

--- programming_lab/vpl_profile/forms.py
+++ programming_lab/vpl_profile/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 from registration.forms import RegistrationForm
 
 class VPLRegistrationForm(RegistrationForm):
@@ -8,3 +9,13 @@ class VPLRegistrationForm(RegistrationForm):
 
     def save(self):
         super(VPLRegistrationForm, self).save(commit=False)
+
+    def clean_email(self):
+        """
+        Validate that the supplied email address is unique for the
+        site.
+        
+        """
+        if User.objects.filter(email__iexact=self.cleaned_data['email']):
+            raise forms.ValidationError("This email address is already in use. Please supply a different email address.")
+        return self.cleaned_data['email']
